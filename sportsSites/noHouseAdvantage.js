12b7c159f1dfb91c9731beb980bbdc987c43147f
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-07-21 00:01:38 -0400

    removes unnecessary comments

--- sportsSites/noHouseAdvantage.js
+++ sportsSites/noHouseAdvantage.js
@@ -11,9 +11,7 @@ puppeteer.use(StealthPlugin());
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
-        executablePath: executablePath(),
         defaultViewport: false,
-        userDataDir: "./tmp"
     });
     
     //Opens page on the browser
@@ -75,36 +73,15 @@ puppeteer.use(StealthPlugin());
 })()
 
 async function login(page) {
-    //await page.evaluateOnNewDocument(function () {
-    //    navigator.geolocation.getCurrentPosition = function (cb) {
-    //        setTimeout(() => {
-    //            cb({
-    //                'coords': {
-    //                    accuracy: 21,
-    //                    altitude: null,
-    //                    altitudeAccuracy: null,
-    //                    heading: null,
-    //                    latitude: 40.712776,
-    //                    longitude: -74.005974,
-    //                    speed: null
-    //                }
-    //            })
-    //        }, 1000)
-    //    }
-    //});
-
-    //Goes to a website of your choosing
-    if (false) {
         await page.goto('https://web.nohouseadvantage.com/login', { waitUntil: 'networkidle0' });
 
         await page.waitForSelector(".mat-form-field-flex")
 
         await page.type('.mat-input-element.mat-form-field-autofill-control.ng-untouched.ng-pristine.ng-invalid.cdk-text-field-autofill-monitored.ng-star-inserted', process.env.NHA_USERNAME)
-        await page.type(".mat-input-element.mat-form-field-autofill-control.ng-tns-c58-1.ng-untouched.ng-pristine.ng-invalid.cdk-text-field-autofill-monitored", process.env.NHA_PASSWORD)
+        await page.type(".mat-input-element.mat-form-field-autofill-control.ng-tns-c58-1.ng-untouched.ng-pristine.ng-invalid.cdk-text-field-autofill-monitored", "Z938udi19*m#9")
 
         await Promise.all([
             page.click('.btn-send-success.btn-signin.ng-star-inserted'),
             page.waitForNavigation({ waitUntil: 'networkidle0' }),
         ]);
-    }
 }
